98b315f1b40202cd88e446364da0bf0088bbbe3f
luca <alessandri.luca1991@gmail.com>
2022-03-01 09:35:03 +0100

    yeah

--- dockerFIleGen_GUI_V2/dockerFileGenerator.sh
+++ dockerFIleGen_GUI_V2/dockerFileGenerator.sh
@@ -1,4 +1,4 @@
-docker build . -t  repbioinfo/dockerfilegeneratorv11
+docker build . -t  repbioinfo/dockerfilegeneratorv12
 if test -f "./configurationFile.txt"; then
     echo "$FILE exists."
 else
@@ -8,4 +8,4 @@ tt=$(head configurationFile.txt)
 mkdir $tt
 cp ./configurationFile.txt $tt
 rm $tt/id.txt
-docker run -itv $tt:/sharedFolder -v /var/run/docker.sock:/var/run/docker.sock --cidfile  $tt/id.txt --privileged=true -p  3000:3000 repbioinfo/dockerfilegeneratorv11
+docker run -itv $tt:/sharedFolder -v /var/run/docker.sock:/var/run/docker.sock --cidfile  $tt/id.txt --privileged=true -p  3000:3000 repbioinfo/dockerfilegeneratorv12
