118632d4d63b928bdc27c92c9734f8ed22b0d2b0
alessandriLuca <alessandri.luca1991@gmail.com>
2022-03-03 23:28:25 +0100

    Update dockerFileGenerator.cmd

--- dockerFIleGen_GUI_V2/dockerFileGenerator.cmd
+++ dockerFIleGen_GUI_V2/dockerFileGenerator.cmd
@@ -1,8 +1,8 @@
-docker build . -t  repbioinfo/dockerfilegeneratorv10
+docker build . -t  repbioinfo/dockerfilegeneratorv13
 @Set "Build=%CD%"
 @Echo(%Build%
 @If Not Exist "configurationFile.txt" Set /P "=%Build%" 0<NUL 1>"configurationFile.txt"
 mkdir %Build%
 copy configurationFile.txt %Build%
 del %Build%\id.txt
-docker run -itv %Build%:/sharedFolder/ -v /var/run/docker.sock:/var/run/docker.sock --privileged=true --cidfile  %Build%\id.txt -p 3000:3000   -e DISABLE_AUTH=true repbioinfo/dockerfilegeneratorv10
+docker run -itv %Build%:/sharedFolder/ -v /var/run/docker.sock:/var/run/docker.sock --privileged=true --cidfile  %Build%\id.txt -p 3000:3000   -e DISABLE_AUTH=true repbioinfo/dockerfilegeneratorv13
